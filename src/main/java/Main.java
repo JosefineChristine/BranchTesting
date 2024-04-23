586bd3f8655e3880fc649c5527dd506557128229
JosefineChristine <joro0003@stud.kea.dk>
2024-04-23 15:41:14 +0200

    showMood tilføjet i main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,21 @@ public class Main {
         Cat cat = new Cat();
         Dog dog = new Dog();
 
-        System.out.println("cat" + dog.makeSound());
-        System.out.println("dog" + cat.makeSound());
+        System.out.println("Katten siger " + dog.makeSound());
+        System.out.println("Hunden siger " + cat.makeSound());
+        System.out.println();
 
         cat.eat("cat food");
         dog.eat("dog food");
+
+        System.out.println(dog.showMood(true));
+        System.out.println(dog.showMood(false));
+        System.out.println();
+        System.out.println(cat.showMood(true));
+        System.out.println(cat.showMood(false));
 
         }
 
     }
 
 
-
-}
